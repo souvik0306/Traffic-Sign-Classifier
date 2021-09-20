d64d1263ad26b55aa6c231f72e49fb75a10c432c
souvik0306 <souvikdatta123@gmail.com>
2021-09-20 21:31:32 +0530

    Contours

--- object_tracking/test.py
+++ object_tracking/test.py
@@ -1,25 +1,26 @@
 import cv2 as cv
 
 cap = cv.VideoCapture(r'D:\Autonomous-Guided-Vehicle\Resources\highway.mp4')
-out = cv.VideoWriter('mask_output.mp4',cv.VideoWriter_fourcc(*'XVID'), 10, (700,500))
+# out = cv.VideoWriter('mask_output.mp4',cv.VideoWriter_fourcc(*'XVID'), 10, (700,500))
 
 object_detector = cv.createBackgroundSubtractorMOG2()
 while True:
     ret, frame = cap.read()
-    width,height = 700,500
+    width,height = 700,500 #resizing 
     frame = cv.resize(frame,(width,height))
+    mask = object_detector.apply(frame) 
 
-    mask = object_detector.apply(frame)
-    new = cv.cvtColor(mask, cv.COLOR_GRAY2RGB)
+    countours, _ = cv.findContours(mask,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE) 
+    for cnt in countours:
+        area = cv.contourArea(cnt)
+        if area > 150:  #150 pixels
+            cv.drawContours(frame,[cnt],-1,(0,255,0),thickness=2)
 
-    out.write(new)
-    cv.imshow("Mask",mask)
-    #cv.imshow("Frame",frame)
+    cv.imshow("Mask",frame)
     key = cv.waitKey(30)
     if key == 27:
         break
 
 cap.release()
 cv.destroyAllWindows()
-out.release()
 
