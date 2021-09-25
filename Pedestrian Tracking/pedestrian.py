a9b6e9d007a418edc4a5ab4597b1748df7f1bf6e
souvik0306 <souvikdatta123@gmail.com>
2021-09-25 11:09:17 +0530

    MP4 Video

--- Pedestrian Tracking/pedestrian.py
+++ Pedestrian Tracking/pedestrian.py
@@ -4,6 +4,7 @@ import numpy as np
 body_classifier = cv.CascadeClassifier(r'D:\Autonomous-Guided-Vehicle\Pedestrian Tracking\haarcascade_fullbody.xml')
 
 cap = cv.VideoCapture(r'D:\Autonomous-Guided-Vehicle\Resources\street2.wmv')
+out = cv.VideoWriter('Pedestrian.mp4', cv.VideoWriter_fourcc(*'m', 'p', '4', 'v'), 20, (700, 500))
 
 while cap.isOpened():
     _,frame = cap.read()
@@ -15,7 +16,10 @@ while cap.isOpened():
     for (x,y,w,h) in bodies:
         cv.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
         cv.imshow("Frames",frame)
+        out.write(frame)
 
     if cv.waitKey(1) == 13:
         break
 cv.destroyAllWindows()
+cap.release()
+out.release()
